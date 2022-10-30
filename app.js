a9389e6a071e77af36ebd18b98978d9e141c42f4
Chalon Lubin <chalonlubin1@gmail.com>
2022-10-30 00:39:31 -0700

    format cleanup

--- app.js
+++ app.js
@@ -1,56 +1,50 @@
+"use strict";
 
-
-const form = document.querySelector('form');
-
-
+const form = document.querySelector("form");
 
 // Form input function
-
-form.addEventListener("submit", function(e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const imgUrl = form.querySelector('#image-url');
-  const topText = form.querySelector('#top-text');
-  const botText = form.querySelector('#bottom-text');
-  const memeHost = document.querySelector('#meme-div');
-  const formInputs = form.querySelectorAll('.user');
+  const imgUrl = form.querySelector("#image-url");
+  const topText = form.querySelector("#top-text");
+  const botText = form.querySelector("#bottom-text");
+  const memeHost = document.querySelector("#meme-div");
+  const formInputs = form.querySelectorAll(".user");
 
   // Imaage Input
-  const img = document.createElement('img');
-  img.classList.add("card-img")
+  const img = document.createElement("img");
+  img.classList.add("card-img");
   img.src = imgUrl.value;
   // Top Text
-  const top = document.createElement('p');
-  top.classList.add("card-text","top-card");
-  top.textContent = topText.value
+  const top = document.createElement("p");
+  top.classList.add("card-text", "top-card");
+  top.textContent = topText.value;
   // Bottom text
   const bot = document.createElement("p");
   bot.classList.add("card-text", "bot-card");
-  bot.textContent = botText.value
-
-   // Card body creation
-   const cardBody = document.createElement("div");
-    cardBody.classList.add("card-img-overlay");
-    cardBody.appendChild(top);
-    cardBody.appendChild(bot);
-  // card creation
+  bot.textContent = botText.value;
+  // Card body creation
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-img-overlay");
+  cardBody.appendChild(top);
+  cardBody.appendChild(bot);
+  // Card container creation + Appendages
   const card = document.createElement("div");
   card.classList.add("card");
   memeHost.appendChild(card);
   card.appendChild(img);
   card.appendChild(cardBody);
-
-  formInputs.forEach(input => {
-    input.value = '';
+  // Clear input fields
+  formInputs.forEach((input) => {
+    input.value = "";
   });
 });
 
-document.addEventListener("reset", function(e){
+// Reset button to remove ALL memes
+document.addEventListener("reset", function (e) {
   e.preventDefault();
-  const memeHost = document.querySelector('#meme-div');
+  const memeHost = document.querySelector("#meme-div");
   console.log("its clicking");
   memeHost.innerHTML = "";
-})
-
-
-
+});
