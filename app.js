e7d8f43d32efb03ba20ab25446e4bb93b9cb3667
Chalon Lubin <chalonlubin1@gmail.com>
2022-11-05 23:50:39 -0700

    Update to style and added text slider

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ form.addEventListener("submit", function (e) {
   const botText = form.querySelector("#bottom-text");
   const memeHost = document.querySelector("#meme-div");
   const formInputs = form.querySelectorAll(".user");
+  const rangeValue = form.querySelector("#font-size").value;
 
   // Imaage Input
   const img = document.createElement("img");
@@ -20,10 +21,12 @@ form.addEventListener("submit", function (e) {
   const top = document.createElement("p");
   top.classList.add("card-text", "top-card");
   top.textContent = topText.value;
+  top.style.fontSize = rangeValue + "px";
   // Bottom text
   const bot = document.createElement("p");
   bot.classList.add("card-text", "bot-card");
   bot.textContent = botText.value;
+  bot.style.fontSize = rangeValue + "px";
   // Card body creation
   const cardBody = document.createElement("div");
   cardBody.classList.add("card-img-overlay");
@@ -41,9 +44,19 @@ form.addEventListener("submit", function (e) {
   });
 });
 
+const rangeSlider = form.querySelector("#font-size");
+
+rangeSlider.addEventListener("click", function (e) {
+  const fontLabel = form.querySelector("#font-size-label");
+  const rangeValue = form.querySelector("#font-size").value;
+  e.preventDefault();
+  fontLabel.innerText = `Text Size: ${rangeValue} px`;
+  fontLabel.style.fontSize = rangeValue + "px";
+});
+
 // Reset button to remove ALL memes
 document.addEventListener("reset", function (e) {
   e.preventDefault();
-  const memeHost = document.querySelector("#meme-div");
-  memeHost.innerHTML = "";
+  const memeHost = document.querySelector("#meme-div").lastChild;
+  memeHost.remove();
 });
